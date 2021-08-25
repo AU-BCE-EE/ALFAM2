29e6f622635402ef5a44ea61736b7856fbae64bb
Sasha D. Hafner <sasha.hafner@gmail.com>
2021-08-25 14:14:58 -0400

    Create par data objects

---
+++ make-Rda/make_Rda.R
@@ -0,0 +1,10 @@
+# Creates parameter objects
+
+pars01 <- read.csv('pars_set1.csv', row.names = 1, header = FALSE)
+pars01 <- as.matrix(pars01)[, 1]
+save(pars01, file = '../data/pars01.rda')
+
+pars02 <- read.csv('pars_set2.csv', row.names = 1, header = FALSE)
+pars02 <- as.matrix(pars02)[, 1]
+save(pars02, file = '../data/pars02.rda')
+
