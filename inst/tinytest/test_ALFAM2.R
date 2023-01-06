fffdb3be9e01871a5bf87b0d9e984b5d2c825c86
Sasha D. Hafner <sasha@hafnerconsulting.com>
2023-01-06 09:17:10 -0500

    Add couple tests on incorp

--- inst/tinytest/test_ALFAM2.R
+++ inst/tinytest/test_ALFAM2.R
@@ -24,7 +24,27 @@ expect_equal(pred0$er[1], pred1$er[49])
 
 
 # add.incorp.rows adds a row ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# when incorp is before first interval
 dat0 <- data.frame(ctime = 48, TAN.app = 100)
+dat0$incorp.deep <- TRUE
+dat0$t.incorp <- 4
+pred0 <- alfam2(dat0, app.name = 'TAN.app', time.name = 'ctime', time.incorp = 't.incorp', add.incorp.rows = TRUE, warn = FALSE)
+
+expect_equal(length(pred0$er), 2)
+expect_equal(pred0$ct, c(4, 48))
+
+# add.incorp.rows adds a row ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# when incorp is between intervals
+dat0 <- data.frame(ctime = c(2, 48), TAN.app = 100)
+dat0$incorp.deep <- TRUE
+dat0$t.incorp <- 4
+pred0 <- alfam2(dat0, app.name = 'TAN.app', time.name = 'ctime', time.incorp = 't.incorp', add.incorp.rows = TRUE, warn = FALSE)
+
+expect_equal(length(pred0$er), 3)
+expect_equal(pred0$ct, c(2, 4, 48))
+
+# add.incorp.rows does not add a row when incorp time matches interval end ~~~~~~~
+dat0 <- data.frame(ctime = c(4, 48), TAN.app = 100)
 dat0$incorp.deep <- TRUE
 dat0$t.incorp <- 4
 pred0 <- alfam2(dat0, app.name = 'TAN.app', time.name = 'ctime', time.incorp = 't.incorp', add.incorp.rows = TRUE, warn = FALSE)
@@ -64,14 +84,15 @@ dat0 <- data.frame(ctime = 48, TAN.app = 100)
 dat0$`__r1` <- 0
 expect_warning(alfam2(dat0, app.name = "TAN.app", time.name = "ctime", warn = FALSE))
 
-# Make sure add.pars changes output whether overriding or replacing
+# Make sure add.pars changes output whether overriding/replacing or adding/extending
 dat0 <- data.frame(ctime = 48, TAN.app = 100, man.dm = 5, air.temp = 10, wind.2m = 5, soil.type.clay = 1)
 pred0 <- alfam2(dat0, app.name = "TAN.app", time.name = "ctime", warn = FALSE)
+# Override
 pred1 <- alfam2(dat0, add.pars = c(wind.2m.r1 = 1), app.name = "TAN.app", time.name = "ctime", warn = FALSE)
+# Extend
 pred2 <- alfam2(dat0, add.pars = c(soil.type.clay.f0 = 1), app.name = "TAN.app", time.name = "ctime", warn = FALSE)
 expect_false(identical(pred0, pred1))
 expect_false(identical(pred0, pred2))
-
 
 # Tests are needed for groups and pass_cols
 # Also for some warnings (like missing predictor vars, changning centering means. . .
