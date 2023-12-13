1ae2d0d6c8e2c3bd0782f515170f682bbc2b949d
Sasha D. Hafner <sasha@hafnerconsulting.com>
2023-12-13 14:12:50 -0500

    v3.18 w pars03_alpha.
    And pars03var_alpha.

--- make-Rda/make_Rda.R
+++ make-Rda/make_Rda.R
@@ -8,10 +8,10 @@ pars02 <- read.csv('pars_set2.csv', row.names = 1, header = FALSE)
 alfam2pars02 <- ALFAM2pars02 <- as.matrix(pars02)[, 1]
 save(alfam2pars02, ALFAM2pars02, file = '../data/alfam2pars02.rda')
 
-pars03 <- read.csv('pars_set3.csv', row.names = 1, header = FALSE)
-alfam2pars03 <- ALFAM2pars03 <- as.matrix(pars03)[, 1]
-save(alfam2pars03, ALFAM2pars03, file = '../data/alfam2pars03.rda')
+pars03_alpha <- read.csv('pars_set3.csv', row.names = 1, header = FALSE)
+alfam2pars03_alpha <- ALFAM2pars03_alpha <- as.matrix(pars03_alpha)[, 1]
+save(alfam2pars03_alpha, ALFAM2pars03_alpha, file = '../data/alfam2pars03_alpha.rda')
 
-alfam2pars03var <- read.csv('pars_set3_var.csv', row.names = 1, header = TRUE)
-alfam2pars03var <- ALFAM2pars03var <- as.matrix(alfam2pars03var)
-save(alfam2pars03var, ALFAM2pars03var, file = '../data/alfam2pars03var.rda')
+alfam2pars03var_alpha <- read.csv('pars_set3_var.csv', row.names = 1, header = TRUE)
+alfam2pars03var_alpha <- ALFAM2pars03var_alpha <- as.matrix(alfam2pars03var_alpha)
+save(alfam2pars03var_alpha, ALFAM2pars03var_alpha, file = '../data/alfam2pars03var_alpha.rda')
