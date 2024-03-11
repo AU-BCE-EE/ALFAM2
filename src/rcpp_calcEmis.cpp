4c1cde6513c692447faeaf70a3ac3c98ec5ec25f
Sasha D. Hafner <sasha@hafnerconsulting.com>
2024-03-11 15:52:07 -0400

    Change sol eq terms to match paper presentation.
    Should be no effect on results

--- src/rcpp_calcEmis.cpp
+++ src/rcpp_calcEmis.cpp
@@ -67,11 +67,10 @@ List rcpp_calcEmis(const NumericVector cta, const NumericVector F0a,
       
       //Calculate intermediates
       //Calculate pools at *end* of ct[i]
-      F[i] = Fti * exp(-(rf[i]) * ddt[i]);
-      S[i] = exp(-rs[i] * ddt[i]) * (Sti + r2[i] * (1 - exp(-rd[i] * ddt[i])) / rd[i] * Fti);
-      femis = r1[i] * (1 - exp(-rf[i] * ddt[i])) / rf[i] * Fti;
-      em = (Fti + Sti - F[i] - S[i] - femis) / rs[i];
-      semis = r3[i] * em;
+      F[i] = Fti * exp(-rf[i] * ddt[i]);
+      S[i] = exp(-rs[i] * ddt[i]) * (Sti + r2[i] * Fti * (1 - exp(-rd[i] * ddt[i])) / rd[i]);
+      femis = r1[i] / rf[i] * Fti * (1 - exp(-rf[i] * ddt[i]));
+      semis = r3[i] / rs[i] * (Fti + Sti - F[i] - S[i] - femis)
       E[i] = Eti + femis + semis;
 
       //save pools for next step
