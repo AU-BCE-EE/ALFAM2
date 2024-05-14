0df9c209373d0aba352dfdeed8dbbdc7d528a29f
Sasha D. Hafner <sasha@hafnerconsulting.com>
2024-05-14 14:58:44 -0400

    Fix prepDum.
    Stupid use of colSums instead of rowSums

--- R/checkDum.R
+++ R/checkDum.R
@@ -5,8 +5,8 @@ checkDum <- function(dat, vnames = c('^app\\.mthd\\.[ocbt]', '^incorp\\.[ds]', '
   for (i in vnames) {
     dd <- dat[, grepl(i, names(dat))]
     if (ncol(dd) > 0) {
-      cs <- colSums(dd)
-      if (any(cs > 1)) {
+      rs <- rowSums(dd)
+      if (any(rs > 1)) {
         return(1)
       } else {
         return(0)
