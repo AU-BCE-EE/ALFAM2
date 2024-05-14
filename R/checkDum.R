b29136a315d8463bc2c11b8bf0d9d300cc42613a
Sasha D. Hafner <sasha@hafnerconsulting.com>
2024-05-14 15:18:12 -0400

    Fix another problem with new checkDum

--- R/checkDum.R
+++ R/checkDum.R
@@ -5,7 +5,7 @@ checkDum <- function(dat, vnames = c('^app\\.mthd\\.[ocbt]', '^incorp\\.[ds]', '
 
   for (i in vnames) {
     dd <- dat[, grepl(i, names(dat))]
-    if (ncol(dd) > 0) {
+    if (any(dd > 0)) {
       rs <- rowSums(dd)
       if (any(rs > 1)) {
         return(1)
