ce32fc0285d8a37b764834137bd641df1c560fec
Sasha D. Hafner <sasha@hafnerconsulting.com>
2024-06-03 15:45:04 -0400

    rm some clinging _alpha

--- make-Rda/make_Rda.R
+++ make-Rda/make_Rda.R
@@ -9,9 +9,9 @@ alfam2pars02 <- as.matrix(pars02)[, 1]
 save(alfam2pars02, file = '../data/alfam2pars02.rda')
 
 pars03 <- read.csv('pars_set3.csv', row.names = 1, header = FALSE)
-alfam2pars03 <- as.matrix(pars03_alpha)[, 1]
+alfam2pars03 <- as.matrix(pars03)[, 1]
 save(alfam2pars03, file = '../data/alfam2pars03.rda')
 
-alfam2pars03var_alpha <- read.csv('pars_set3_var.csv', header = TRUE)
-alfam2pars03var_alpha <- as.matrix(alfam2pars03var_alpha)
-save(alfam2pars03var_alpha, file = '../data/alfam2pars03var.rda')
+alfam2pars03var <- read.csv('pars_set3_var.csv', header = TRUE)
+alfam2pars03var <- as.matrix(alfam2pars03var)
+save(alfam2pars03var, file = '../data/alfam2pars03var.rda')
